c6fad189f432bd3b1baa7106f3b6e973047dbf5a
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-06-30 17:45:44 -0400

    Parse from a string

--- Sources/CurrencyDecimal/CurrencyDecimal.swift
+++ Sources/CurrencyDecimal/CurrencyDecimal.swift
@@ -1,4 +1,64 @@
-struct CurrencyDecimal {
-
-    var text = "Hello, World!"
+extension Character {
+    var isDigit: Bool {
+        return self >= "0" && self <= "9"
+    }
+    
+    var isMinusSign: Bool {
+        return self == "-"
+    }
+    
+    var isDecimalPoint: Bool {
+        return self == "."
+    }
 }
+
+struct CurrencyDecimal {
+    let beforeDecimal: String
+    let afterDecimal: String
+    
+    init?(string: String) {
+        var current = string.startIndex
+        
+        @discardableResult func advance() -> Character {
+            guard current < string.endIndex
+                else { return "\0" }
+            let cc = string[current]
+            current = string.index(after: current)
+            return cc
+        }
+        
+        @discardableResult func peek() -> Character {
+            let peekIdx = string.index(after: current)
+            guard peekIdx < string.endIndex else { return "\0" }
+            return string[peekIdx]
+        }
+        
+        // first character can be minus sign
+        if peek().isMinusSign { advance() }
+        
+        // as many digits as possible
+        while peek().isDigit { advance() }
+        
+        // digits before decimal, incluing minus sign
+        beforeDecimal = String(string[...current])
+        
+        // skip over decimal, if present
+        if peek().isDecimalPoint { advance() }
+        
+        // as many digits as possible, after decimal sign
+        let afterDecimalStartIdx = string.index(after: current)
+        while peek().isDigit { advance() }
+        
+        afterDecimal = String(string[afterDecimalStartIdx...current])
+        
+        // if not at the end of the string now, failed to parse a decimal
+        if peek() != "\0" {
+            return nil
+        }
+    }
+    
+    var stringValue: String {
+        return "\(beforeDecimal).\(afterDecimal)"
+    }
+}
+
--- Tests/CurrencyDecimalTests/CurrencyDecimalTests.swift
+++ Tests/CurrencyDecimalTests/CurrencyDecimalTests.swift
@@ -2,15 +2,12 @@ import XCTest
 @testable import CurrencyDecimal
 
 class CurrencyDecimalTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CurrencyDecimal().text, "Hello, World!")
+    func testOneDecimalPlace() {
+        XCTAssertEqual(CurrencyDecimal(string: "11.2")?.stringValue, "11.2")
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testOneDecimalPlace", testOneDecimalPlace),
     ]
 }
