7f6845c12e0dea60d033366b6e4c23d3001003f2
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-06-30 22:18:38 -0400

    More unit tests

--- Tests/CurrencyDecimalTests/CurrencyDecimalTests.swift
+++ Tests/CurrencyDecimalTests/CurrencyDecimalTests.swift
@@ -9,10 +9,24 @@ class CurrencyDecimalTests: XCTestCase {
     func testOneDecimalPlace() {
         XCTAssertEqual(CurrencyDecimal(string: "11.2")?.description, "11.200")
     }
-
-
-    static var allTests = [
-        ("testNoDecimalPlaces", testNoDecimalPlaces),
-        ("testOneDecimalPlace", testOneDecimalPlace),
-    ]
+    
+    func testMultipleDecimals() {
+        XCTAssertEqual(CurrencyDecimal(string: "1..1")?.description, nil)
+    }
+    
+    func testNegativeNumber() {
+        XCTAssertEqual(CurrencyDecimal(string: "-1")?.description, "-1.000")
+    }
+    
+    func testLiteralInteger() {
+        let cd: CurrencyDecimal = 11
+        XCTAssertEqual(cd.description, "11.000")
+    }
+    
+    func testAdd() {
+        let first = CurrencyDecimal(string: "59.99")!
+        let second = CurrencyDecimal(string: "4.99")!
+        
+        XCTAssertEqual(first + second, CurrencyDecimal(string: "64.98")!)
+    }
 }
